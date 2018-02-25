7432f13a9dbf9a5de4389fdbeda8450c45148f11
honeyTi <326009895@qq.com>
2018-02-25 15:18:21 +0800

    画布完善

--- js/panorama.js
+++ js/panorama.js
@@ -10,6 +10,7 @@ panorama.prototype = {
 
 // 数据页面获取初始化方法
     init: function (data,type) {
+        
         'use strict';
         const container = data.container,
               color = data.color,
@@ -20,23 +21,49 @@ panorama.prototype = {
         if (type === "neo4jData") {
             mapData = data.data;
         } else {
-
+            
         }
+        // 用到的变量
+        let svg, svgScale, svgTranslate, svgRelationships, svgNodes;
+        
         // 页面处理，添加d3元素，增加内容
         function appendGraph(container) {
+            console.log('创建画布元素');
             svg = container.append('svg')
                 .attr('width', '100%')
                 .attr('height', '100%')
                 .call(d3.zoom().on('zoom', function () {
-                    let scale = d3.event.transform.k,
+                    var scale = d3.event.transform.k,
                         translate = [d3.event.transform.x, d3.event.transform.y];
+
+                    if (svgTranslate) {
+                        translate[0] += svgTranslate[0];
+                        translate[1] += svgTranslate[1];
+                    }
+
+                    if (svgScale) {
+                        scale *= svgScale;
+                    }
+
+                    svg.attr('transform', 'translate(' + translate[0] + ', ' + translate[1] + ') scale(' + scale + ')');
                 }))
+                .on('dblclick.zoom', null)
+                .append('g')
+                .attr('width', '100%')
+                .attr('height', '100%');
+            
+            svgRelationships = svg.append('g')
+                .attr('class', 'relationships');
+
+            svgNodes = svg.append('g')
+                .attr('class', 'nodes');
         }
+        appendGraph(d3.select(container));
     },
 
 // 事件方法
     nodeClick: function () {
-
+        
     },
 
 // 鼠标移入事件方法
