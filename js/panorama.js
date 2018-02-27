dd79bf6921bf1e32208d4f301b1b19fe9ce1bf93
xiaomeiqi <xiaomeiqi@dataocean.com.cn>
2018-02-27 17:58:55 +0800

    色值列表

--- js/panorama.js
+++ js/panorama.js
@@ -52,7 +52,29 @@ panorama.prototype = {
             if (option.color.length > 0) {
                 color = option.color;
             } else {
-                color = ["#ff461f", "#fff143", "#75664d", "#cca4e3", "#8d4bbb", "#4b5cc4", "#00e500", "#274a78", "#544a47", "#ffb61e", "#0eb83a"];
+                color = [
+                    '#68bdf6', // light blue
+                    '#6dce9e', // green #1
+                    '#faafc2', // light pink
+                    '#f2baf6', // purple
+                    '#ff928c', // light red
+                    '#fcea7e', // light yellow
+                    '#ffc766', // light orange
+                    '#405f9e', // navy blue
+                    '#a5abb6', // dark gray
+                    '#78cecb', // green #2,
+                    '#b88cbb', // dark purple
+                    '#ced2d9', // light gray
+                    '#e84646', // dark red
+                    '#fa5f86', // dark pink
+                    '#ffab1a', // dark orange
+                    '#fcda19', // dark yellow
+                    '#797b80', // black
+                    '#c9d96f', // pistacchio
+                    '#47991f', // green #3
+                    '#70edee', // turquoise
+                    '#ff75ea' // pink
+                ];
             }
             for (let node_index = 0; node_index < nodes.length; ++node_index) {
                 let first_label = nodes[node_index]["data"]["labels"];
